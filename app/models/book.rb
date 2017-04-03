affc698dafd81e7d6613d89b701dfff84e2689f2
Nao Yukimori <sutoradaf2@gmail.com>
2017-04-03 23:14:33 +0900

    default_scopeをコメントで記述これ忘れてると挙動がちがってハマるやつ

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,5 @@
 class Book < ActiveRecord::Base
+  #default_scope -> { order("published_on desc") }
   scope :constly, -> { where("price > ?", 3000) }
   scope :written_about, ->(theme) { where("name like ?", "%#{theme}%") }
 end
