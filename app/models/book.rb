c9265d3ed9d8e3c67c6ef2d36b02ad9f93b65934
Nao Yukimori <sutoradaf2@gmail.com>
2017-04-03 23:27:59 +0900

    多対多のリレーションを作成（中間テーブルを作成するケースはこの時）

--- app/models/book.rb
+++ app/models/book.rb
@@ -3,5 +3,10 @@ class Book < ActiveRecord::Base
   scope :constly, -> { where("price > ?", 3000) }
   scope :written_about, ->(theme) { where("name like ?", "%#{theme}%") }
   
+  # １対多 
   belongs_to :publisher
+  
+  # 多対多
+  has_many :book_authors
+  has_many :authors, through: :book_authors
 end
