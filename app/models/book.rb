13988035c1f0ad732cdf36c803d61773fa15a777
Nao Yukimori <sutoradaf2@gmail.com>
2017-04-03 23:22:26 +0900

    1対多の関係にしてみた

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,4 +2,6 @@ class Book < ActiveRecord::Base
   #default_scope -> { order("published_on desc") }
   scope :constly, -> { where("price > ?", 3000) }
   scope :written_about, ->(theme) { where("name like ?", "%#{theme}%") }
+  
+  belongs_to :publisher
 end
