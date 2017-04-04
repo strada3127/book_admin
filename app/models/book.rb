b25dedb5698651a46a788b4ec90d556e55c6bbc5
Nao Yukimori <sutoradaf2@gmail.com>
2017-04-04 23:21:08 +0900

    カスタムバリデーション

--- app/models/book.rb
+++ app/models/book.rb
@@ -9,4 +9,16 @@ class Book < ActiveRecord::Base
   # 多対多
   has_many :book_authors
   has_many :authors, through: :book_authors
+
+  validates :name, presence: true
+  validates :name, length: { maximum: 15}
+  validates :price, numericality: { greater_than_or_equal_to: 0 }
+  
+  # カスタムバリデーション
+  validate do |book|
+    if book.name.include?("exercise")
+      book.errors[:name] << "I don't like exercise."
+    end
+  end
 end
+
