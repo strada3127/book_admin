cd4bfb85ecf7f7f51e8725f1de84104c4f08e4b5
Nao Yukimori <sutoradaf2@gmail.com>
2017-04-12 23:11:38 +0900

    adminのフラグを検知してパラメータを許可する

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -6,5 +6,17 @@ class ProfileController < ApplicationController
   end
 
   def update
+    user = current_user
+    user.update(user_params)
+  end
+
+  private
+
+  def user_params
+    if current_user.admin?
+      params.require(:user).permit(:name, :email, :admin)
+    else
+      params.require(:user).permit(:name, :email)
+    end
   end
 end
